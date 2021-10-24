863baa93fa2263abde28c39886ad1f694b908aed
Aidan Horemans <aidanjhoremans@gmail.com>
2021-10-23 21:45:47 -0600

    fixed AndroidManifest

--- Ditto_App/app/src/main/java/ditto_app/example/ditto_app/MainActivity.java
+++ Ditto_App/app/src/main/java/ditto_app/example/ditto_app/MainActivity.java
@@ -4,8 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.ditto_app.R;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
