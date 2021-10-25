bd4ec977a626e2493041525d12c6a7c63f02731a
Kelly Shih <kshih@ualberta.ca>
2021-10-24 20:18:55 -0600

    Started, just waiting for xml

--- Ditto/app/src/main/java/com/team11/ditto/MyHabitsActivity.java
+++ Ditto/app/src/main/java/com/team11/ditto/MyHabitsActivity.java
@@ -1,4 +1,27 @@
 package com.team11.ditto;
 
-public class MyHabitsActivity {
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MyHabitsActivity extends AppCompatActivity{
+
+    //Declare variables for the list of habits
+    ListView myHabitList;
+    ArrayAdapter<Habit> habitAdapter;
+    ArrayList<Habit> habitDataList;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myhabits);
+    }
+
+
 }
