711b5beaa21f11b935c1f45ed806945f60a2f5cc
Kelly Shih <kshih@ualberta.ca>
2021-10-24 21:26:54 -0600

    edits to MyHabitsActivity, added new AddHabitFragment

---
+++ Ditto/app/src/main/java/com/team11/ditto/AddHabitFragment.java
@@ -0,0 +1,34 @@
+package com.team11.ditto;
+
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDialogFragment;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class AddHabitFragment extends DialogFragment {
+    private EditText hTitle;
+    private EditText hReason;
+    private EditText hDate;
+    //private OnFragmentInteractionListener listener;
+}
--- Ditto/app/src/main/java/com/team11/ditto/MyHabitsActivity.java
+++ Ditto/app/src/main/java/com/team11/ditto/MyHabitsActivity.java
@@ -1,24 +1,52 @@
 package com.team11.ditto;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 
 public class MyHabitsActivity extends AppCompatActivity{
 
     //Declare variables for the list of habits
-    ListView myHabitList;
+    ListView habitListView;
     ArrayAdapter<Habit> habitAdapter;
     ArrayList<Habit> habitDataList;
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_myhabits);
+
+        habitListView = findViewById(R.id.);
+        habitDataList = new ArrayList<>();
+
+        habitAdapter = new CustomList_Habit(this, habitDataList);
+        habitListView.setAdapter(habitAdapter);
+
+        db = FirebaseFirestore.getInstance();
+        //Get a top level reference to the collection
+        final CollectionReference collectionReference = db.collection("Habit");
+
+        //add habit button action
+        final FloatingActionButton addCityButton = findViewById(R.id.add_med_button);
+        addCityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AddMedFragment(dosage).show(getSupportFragmentManager(), "ADD_MED");
+
+
+            }
+        });
+
     }
 
 }
