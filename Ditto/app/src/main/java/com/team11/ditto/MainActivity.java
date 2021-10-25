c248ec62001064dd4c06815b76a5fb10d7a9cfe9
Aidan Horemans <aidanjhoremans@gmail.com>
2021-10-24 22:01:37 -0600

    created non-working on click listener for bottomnavigation

--- Ditto/app/src/main/java/com/team11/ditto/MainActivity.java
+++ Ditto/app/src/main/java/com/team11/ditto/MainActivity.java
@@ -1,8 +1,11 @@
 package com.team11.ditto;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -10,11 +13,14 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 import java.util.HashMap;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
     FirebaseFirestore db; //when they addcity button we need to dump into db
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +35,21 @@ public class MainActivity extends AppCompatActivity {
 
         //we use a hashmap to store a key-value pair in firestore. Because its NoSQL database
         HashMap<String, String> data = new HashMap<>();
+
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item){
+                switch(item.getItemId()){
+                    case R.id.my_habits:
+                        
+                        return true;
+                }
+                return true;
+            }
+        });
+
     }
 }
