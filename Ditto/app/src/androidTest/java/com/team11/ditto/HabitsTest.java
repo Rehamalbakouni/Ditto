9dd2df04a9f03018de27e6bfd70e69349ea62a6f
Kelly Shih <kshih@ualberta.ca>
2021-11-03 23:19:06 -0600

    espresso testing

--- Ditto/app/src/androidTest/java/com/team11/ditto/HabitsTest.java
+++ Ditto/app/src/androidTest/java/com/team11/ditto/HabitsTest.java
@@ -2,31 +2,23 @@ package com.team11.ditto;
 /**
  * Test class for MyHabitActivity. All the UI tests are written here. Espresso test framework is
  used
+ TODO: Future tests:
+ * public void testEditHabitTitle() //tests the edit function which will be implemented
+ * public void testEditHabitDetails() //make sure details can only be tested
+ * check for repeated Habit titles!!!
  */
-import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.swipeLeft;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
+
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-import static org.hamcrest.CoreMatchers.anything;
-import static org.hamcrest.CoreMatchers.startsWith;
-import static org.junit.Assert.*;
-import android.app.Activity;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.ListView;
-
-import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.rule.ActivityTestRule;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.junit.After;
 import org.junit.Before;
@@ -40,6 +32,13 @@ public class HabitsTest {
     @Rule
     public ActivityScenarioRule<MyHabitActivity> activityRule = new ActivityScenarioRule<>(MyHabitActivity.class);
 
+
+
+
+
+    /**
+     * Add to all fields of the add fragment
+     */
     @Test
     public void testAddHabit() {
         String title = "Running";
@@ -58,13 +57,106 @@ public class HabitsTest {
         onView(withText("ADD")).perform(click());
 
 
-
     }
 
+
+    /**
+     * Test the swipe action to delete
+     */
     @Test
     public void testDeleteHabit() {
-        //onView(withId(R.id.list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, swipeLeft()));
+        //first add a habit then delete
+        String title = "Read a book";
+        String reason = "Become literate";
+
+        //open the add habit fragment
+        onView(withId(R.id.add_habit)).perform(click());
+
+        //type in title and reason
+        onView(withId(R.id.title_editText)).perform(typeText(title));
+        onView(withId(R.id.reason_editText)).perform(typeText(reason));
+        onView(withId(R.id.tuesday_select)).perform(click());
+        onView(withId(R.id.saturday_select)).perform(click());
+
+        //click add
+        onView(withText("ADD")).perform(click());
+
+        //the delete habit
+        onView(withId(R.id.list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, swipeLeft()));
 
     }
+
+    /**
+     * just add the title test
+     */
+    @Test
+    public void testAddTitle() {
+        String title = "Eating";
+
+        //open the add habit fragment
+        onView(withId(R.id.add_habit)).perform(click());
+
+        //type in title and reason
+        onView(withId(R.id.title_editText)).perform(typeText(title));
+
+        //click add
+        onView(withText("ADD")).perform(click());
+
+    }
+
+    /**
+     * TODO: Testing unable to add without a habit title
+     */
+    @Test
+    public void testNoTitleAdd() {
+        String reason = "To get abs";
+
+        //open the add habit fragment
+        onView(withId(R.id.add_habit)).perform(click());
+
+        //type in title and reason
+        onView(withId(R.id.reason_editText)).perform(typeText(reason));
+        onView(withId(R.id.tuesday_select)).perform(click());
+        onView(withId(R.id.saturday_select)).perform(click());
+
+        //click add
+        onView(withText("ADD")).perform(click());
+
+    }
+
+    /**
+     * test the view habit action -> click on a habit and go to a new activity
+     */
+    @Test
+    public void testViewHabit() {
+        //first add a habit then view it
+        String title = "Massage";
+        String reason = "Stress relief";
+
+        //open the add habit fragment
+        onView(withId(R.id.add_habit)).perform(click());
+
+        //type in title and reason
+        onView(withId(R.id.title_editText)).perform(typeText(title));
+        onView(withId(R.id.reason_editText)).perform(typeText(reason));
+        onView(withId(R.id.tuesday_select)).perform(click());
+        onView(withId(R.id.saturday_select)).perform(click());
+
+        //click add
+        onView(withText("ADD")).perform(click());
+
+        //click and view activity for habit
+        onView(withId(R.id.list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+    }
+
+
+    /*
+    Future tests:
+    public void testEditHabitTitle() //tests the edit function which will be implemented
+    public void testEditHabitDetails() //make sure details can only be tested
+
+     */
+
+
 
 }
