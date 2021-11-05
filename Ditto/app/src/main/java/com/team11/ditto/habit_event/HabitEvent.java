b893fd783c5c9d361add9c1bbd4328f3f3a33f35
vivek-rgb <malhotra@ualberta.ca>
2021-11-05 14:59:32 -0600

    add license

--- Ditto/app/src/main/java/com/team11/ditto/habit_event/HabitEvent.java
+++ Ditto/app/src/main/java/com/team11/ditto/habit_event/HabitEvent.java
@@ -1,3 +1,17 @@
+/** Copyright [2021] [Reham Albakouni, Matt Asgari Motlagh, Aidan Horemans, Courtenay Laing-Kobe, Vivek Malhotra, Kelly Shih]
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 package com.team11.ditto.habit_event;
 
 import java.io.Serializable;
