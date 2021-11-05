cd92c917472bc224cc868f7a06dc0b776d0ffc13
Courtenay Laing-Kobe <laingkob@ualberta.ca>
2021-11-05 03:24:04 -0600

    Done comments. I think

--- Ditto/app/src/main/java/com/team11/ditto/profile_details/SearchList.java
+++ Ditto/app/src/main/java/com/team11/ditto/profile_details/SearchList.java
@@ -15,23 +15,38 @@ import com.team11.ditto.R;
 
 import java.util.ArrayList;
 
+/**
+ * Custom list view for User search to send follow requests
+ * @author Vivek Malhotra
+ */
 public class SearchList extends ArrayAdapter<User> {
 
+    //Declarations
     private ArrayList<User> users;
     private Context context;
 
+    /**
+     * Constructor for custom list
+     * @param context activity context
+     * @param users users to display
+     */
     public SearchList(Context context, ArrayList<User> users) {
         super(context,0,users);
         this.users = users;
         this.context = context;
     }
 
+    /**
+     * Creates a view for a custom list item
+     * @param position position in the list
+     * @param convertView Android default
+     * @param parent Android default
+     * @return custom view for list item
+     */
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @Nullable ViewGroup parent) {
-        //return super.getView(position, convertView, parent);
-
-        View view = convertView;
+         View view = convertView;
 
         if(view == null){
             view = LayoutInflater.from(context).inflate(R.layout.search_content, parent,false);
@@ -43,6 +58,7 @@ public class SearchList extends ArrayAdapter<User> {
         ImageView userphoto = view.findViewById(R.id.search_user_photo);
 
         username.setText(user.getUsername());
+        //TODO implement actual profile photos
         userphoto.setImageResource(R.drawable.background);
 
         return view;
--- Ditto/app/src/main/java/com/team11/ditto/profile_details/SearchUserActivity.java
+++ Ditto/app/src/main/java/com/team11/ditto/profile_details/SearchUserActivity.java
@@ -17,24 +17,39 @@ import com.team11.ditto.interfaces.SwitchTabs;
 import java.util.ArrayList;
 import java.util.Locale;
 
+/**
+ * Activity where the ActiveUser can search for other Users in the db to request to follow
+ * @author Vivek Malhotra
+ */
 public class SearchUserActivity extends AppCompatActivity implements SwitchTabs {
 
+    //Declarations
     private TabLayout tabLayout;
     private ListView user_listView;
-    private static ArrayAdapter searchAdapter;
+    private static ArrayAdapter<User> searchAdapter;
     private ArrayList<User> userDataList;
     private SearchView searchView;
     private ArrayList<String> usernames;
+
+    /**
+     * Instructions on creating the Activity
+     * -search bar at top
+     * -list of search results below
+     * -tabs at bottom
+     * -refresh search results if search bar text changes
+     * @param savedInstanceState current state
+     */
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_users);
-        user_listView = (ListView) findViewById(R.id.search_list_custom);
-        searchView = (SearchView) findViewById(R.id.search_user);
-        tabLayout =(TabLayout) findViewById(R.id.tabs);
+        user_listView = findViewById(R.id.search_list_custom);
+        searchView = findViewById(R.id.search_user);
+        tabLayout = findViewById(R.id.tabs);
 
         usernames = new ArrayList<>();
-        // Just to check if view works as intended
+        //For Prototype display only
+        //TODO implement User search
         User Aidan = new User("Aidan","1346789",25);
         usernames.add("Aidan");
         User Courtenay = new User("Courtenay","123456",25);
@@ -65,7 +80,6 @@ public class SearchUserActivity extends AppCompatActivity implements SwitchTabs 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
-
                 return false;
             }
 
@@ -84,9 +98,12 @@ public class SearchUserActivity extends AppCompatActivity implements SwitchTabs 
         });
     }
 
+    /**
+     * Define behaviour when back button pressed:
+     * -go back to ActiveUser profile
+     */
     @Override
     public void onBackPressed() {
-
         Intent intent = new Intent(getApplicationContext(), UserProfileActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK |  Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
