3e4b7c0624618ec9cdb4220d8b8c1e43bea0a2b4
vivek-rgb <malhotra@ualberta.ca>
2021-11-12 22:16:06 -0700

    Aidan fixed null list error for sent requests

--- Ditto/app/src/main/java/com/team11/ditto/profile_details/SearchUserActivity.java
+++ Ditto/app/src/main/java/com/team11/ditto/profile_details/SearchUserActivity.java
@@ -241,8 +241,10 @@ public class SearchUserActivity extends AppCompatActivity implements SwitchTabs,
             if(task.isSuccessful()){
                 DocumentSnapshot documentSnapshot = task.getResult();
                 List<String> listSent = (List<String>) documentSnapshot.get("sent_requests");
-                if(listSent.size()>0){
-                    sentRequest.addAll(listSent);
+                if(listSent != null){
+                    if(listSent.size()>0){
+                        sentRequest.addAll(listSent);
+                    }
                 }
 
                 Log.d("THIS IS THE DATA ",sentRequest.toString() );
